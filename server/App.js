a92622862301821a187839b1cccd716faf96196f
Jonbodoe <le0220@mail.philau.edu>
2021-02-07 16:03:04 -0500

    refactoring routes in server-side

--- server/App.js
+++ server/App.js
@@ -3,6 +3,9 @@ const express = require('express');
 const cors = require('cors')
 const path = require('path');
 const app = express();
+//  router = express.Router()
+const incidents = require('./routes/incidents.js');
+const apis = require('./routes/apis')
 
 app.use(express.urlencoded());
 app.use(express.json())
@@ -11,21 +14,11 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  res.send('helllooooooo')
+  res.send('helllooooooo world');
 });
 
-app.get('/api', function (req, res) {
-  //  res.send('hellooo')
-   var MongoClient = require('mongodb').MongoClient
-   MongoClient.connect('mongodb://localhost:27017/Test', function (err, client) {
-   //   if (err) throw err
-     var db = client.db('Test')  
-     db.collection('TestingDB').find().toArray(function (err, result) {
-       // if (err) throw err
-       res.json(result);
-     })
-})
+app.use('/incidents', incidents);
 
-})
+app.use('/api', apis);
 
 app.listen(process.env.PORT || 8080);
---
+++ server/routes/incidents.js
@@ -0,0 +1,52 @@
+"use-strict"
+const express = require('express');
+let router = express.Router();
+
+router.route('/').get((req, res) => {
+    res.send('incidents parent!')
+})
+
+router
+    .route('/get-incidents-db')
+    .get((req, res) => {
+        res.send('helllo from get')
+    })
+    .post((req, res) => {
+        var MongoClient = require('mongodb').MongoClient
+        MongoClient.connect('mongodb://localhost:27017/Test', function (err, client) {
+            if (err) throw err
+            var db = client.db('Test')
+            db.collection('TestingDB').find().toArray(function (err, result) {
+                if (err) throw err
+                res.json(result);
+            })
+        })
+    });
+
+// function incidents() {
+//     router.get('/getIncidentsDb', function (req, res) {
+//         var MongoClient = require('mongodb').MongoClient
+//         MongoClient.connect('mongodb://localhost:27017/Test', function (err, client) {
+//             if (err) throw err
+//             var db = client.db('Test')
+//             db.collection('TestingDB').find().toArray(function (err, result) {
+//                 if (err) throw err
+//                 res.json(result);
+//             })
+
+//         })
+//     })
+// }
+
+module.exports = router;
+
+
+  // var MongoClient = require('mongodb').MongoClient
+  // MongoClient.connect('mongodb://localhost:27017/Test', function (err, client) {
+  //   if (err) throw err
+  //   var db = client.db('Test')
+  //   db.collection('TestingDB').find().toArray(function (err, result) {
+  //     if (err) throw err
+  //     res.json(result);
+  //   })
+  // })
