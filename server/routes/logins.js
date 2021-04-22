f6aceeb1b45be401df37227ffd8c17179eb9bb55
Jonbodoe <le0220@mail.philau.edu>
2021-04-21 21:14:54 -0400

    cors changes for heroku deploy

--- server/routes/logins.js
+++ server/routes/logins.js
@@ -11,8 +11,9 @@ router.route('/').get((req, res) => {
 
 router.route('/get-logins-db')
     .get((req, res) => {
-        res.send(jsonData)
         console.log('getting logins data')
+        res.json(jsonData)
+        // console.log('getting logins data')
         
 
         // MAKE SURE TO CHANGE OUT TO LOGIN COLLECTION DB:
